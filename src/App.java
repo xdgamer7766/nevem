66f960d4fa3e989a8dde1a82b3162f5f11679f56
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-09-07 21:19:59 +0200

    spelling

--- src/App.java
+++ src/App.java
@@ -2,7 +2,9 @@ public class App {
 
 
     public static void spelling(String word){
-        
+        for (int i = 0; i < word.length(); i++) {
+            System.out.println(word.charAt(i));
+        }
     }
     public static void main(String[] args) {
         
