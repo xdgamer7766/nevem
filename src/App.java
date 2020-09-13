473edc01ce00fef1843f0f5588bcab79b3c8d8b3
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-09-13 19:19:34 +0200

    string rework

--- src/App.java
+++ src/App.java
@@ -5,9 +5,37 @@ public class App {
 
     public static void spelling(String word){
         for (int i = 0; i < word.length(); i++) {
-            System.out.println(word.charAt(i));
+            if (word.charAt(i) == 's' && word.charAt(i+1) == 'z'){               
+                    System.out.println("sz");               
+            }
+             if (word.charAt(i) == 'z' && word.charAt(i+1) == 's'){               
+                    System.out.println("zs");               
+            }
+             if (word.charAt(i) == 't' && word.charAt(i+1) == 'y'){               
+                    System.out.println("ty");               
+            }
+             if (word.charAt(i) == 'n' && word.charAt(i+1) == 'y'){               
+                    System.out.println("ny");               
+            }
+             if (word.charAt(i) == 'l' && word.charAt(i+1) == 'y'){               
+                    System.out.println("ly");               
+            }
+             if (word.charAt(i) == 'g' && word.charAt(i+1) == 'y'){               
+                    System.out.println("gy");               
+            }
+             if (word.charAt(i) == 'c' && word.charAt(i+1) == 's'){               
+                    System.out.println("cs");               
+            }
+             if (word.charAt(i) == 'd' && word.charAt(i+1) == 'z'){               
+                    if (word.charAt(i+2) == 's')
+                        System.out.println("dzs");
+                    else
+                        System.out.println("dz");             
+            }
         }
     }
+  
+//elválasztás az átláthatóság kedvéért
     public static void main(String[] args) {
         f1();
         f2();
