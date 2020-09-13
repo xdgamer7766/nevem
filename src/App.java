4675a293bc477366337c32f3d0d348e112f21b7e
xdgamer7766 <43033742+xdgamer7766@users.noreply.github.com>
2020-09-13 20:46:18 +0200

    spelling 3.0

--- src/App.java
+++ src/App.java
@@ -1,41 +1,109 @@
 import java.util.Scanner;
 
+
 public class App {
 
 
     public static void spelling(String word){
-        for (int i = 0; i < word.length(); i++) {
-            if (word.charAt(i) == 's' && word.charAt(i+1) == 'z'){               
-                    System.out.println("sz");               
-            }
-             if (word.charAt(i) == 'z' && word.charAt(i+1) == 's'){               
-                    System.out.println("zs");               
-            }
-             if (word.charAt(i) == 't' && word.charAt(i+1) == 'y'){               
-                    System.out.println("ty");               
-            }
-             if (word.charAt(i) == 'n' && word.charAt(i+1) == 'y'){               
-                    System.out.println("ny");               
-            }
-             if (word.charAt(i) == 'l' && word.charAt(i+1) == 'y'){               
-                    System.out.println("ly");               
-            }
-             if (word.charAt(i) == 'g' && word.charAt(i+1) == 'y'){               
-                    System.out.println("gy");               
-            }
-             if (word.charAt(i) == 'c' && word.charAt(i+1) == 's'){               
-                    System.out.println("cs");               
-            }
-             if (word.charAt(i) == 'd' && word.charAt(i+1) == 'z'){               
-                    if (word.charAt(i+2) == 's')
-                        System.out.println("dzs");
+        for (int i = 0; i < word.length(); i++) {            
+           char indexchar = word.charAt(i);
+           switch (indexchar){
+               case 'c':
+                    if (word.charAt(i+1) == 's'){
+                        System.out.println("cs");
+                        i = i + 1;
+                    }
                     else
-                        System.out.println("dz");             
-            }
+                    {
+                        System.out.println('c');
+                    }
+                    break;
+                case 'd':
+                    if (word.charAt(i+1) == 'z'){
+                        if (word.charAt(i+2) == 's'){
+                            System.out.println("dzs");
+                            i = i + 2;
+                        }
+                        else{
+                            System.out.println("dz");
+                            i = i + 1;
+                        }
+                    }
+                    else
+                    {
+                        System.out.println('d');
+                    }
+                    break;
+                case 'g':
+                    if (word.charAt(i+1) == 'y'){
+                        System.out.println("gy");
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        System.out.println('g');
+                    }
+                    break;
+                case 'l':
+                    if (word.charAt(i+1) == 'y'){
+                        System.out.println("ly");
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        System.out.println('l');
+                    }
+                    break;
+                case 'n':
+                    if (word.charAt(i+1) == 'y'){
+                        System.out.println("ny");
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        System.out.println('n');
+                    }
+                    break;
+                case 's':
+                    if (word.charAt(i+1) == 'z'){
+                        System.out.println("sz");
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        System.out.println('s');
+                    }
+                    break;
+                case 't':
+                    if (word.charAt(i+1) == 'y'){
+                        System.out.println("ty");
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        System.out.println('t');
+                    }
+                    break;
+                case 'z':
+                    if (word.charAt(i+1) == 's'){
+                        System.out.println("zs");
+                        i = i + 1;
+                    }
+                    else
+                    {
+                        System.out.println('z');
+                    }
+                    break;
+                default:
+                    System.out.println(word.charAt(i));
+                    break;
+           }
         }
     }
   
-//elválasztás az átláthatóság kedvéért
+
+
+
     public static void main(String[] args) {
         f1();
         f2();
